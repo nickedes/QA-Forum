17e45d152fd531e71bd597ebd9920251db2e2655
Nikhil <nikhil.mittal@99acres.com>
2016-06-23 17:08:47 +0530

    Email passwd check from db

--- application/models/Users.php
+++ application/models/Users.php
@@ -5,7 +5,7 @@
 	class Users extends MY_Model
 	{
 		$this->conn_id = parent::get_pdo();
-		
+
 		// private $table_name = "users";
 		function __construct()
 		{	
@@ -21,6 +21,13 @@
 			$affected_rows = $sth->rowCount();
 			print_r($affected_rows);
 			return $affected_rows;
+		}
+
+		function check($data)
+		{
+			$sql = $this->conn_id->query('select * from '.$this->table_name. ' WHERE email = '.$data['email'] ' and passwd = '.$data['password']);
+			$r = $sql->fetchALL(PDO::FETCH_ASSOC);
+			return $r;
 		}
 	}
 ?>
