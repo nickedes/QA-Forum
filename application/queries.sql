955362361f1ea569292b10d7564f43b5711f01f4
Nikhil <nikhil.mittal@99acres.com>
2016-06-23 11:17:25 +0530

    Errors removed in queries

--- application/queries.sql
+++ application/queries.sql
@@ -1,56 +1,56 @@
-create table user (
+create table users (
 	user_id int unsigned AUTO_INCREMENT NOT NULL,
 	name varchar(100) NOT NULL,
 	email varchar(320) unique NOT NULL,          
 	mobileno varchar(10) unique NOT NULL,
 	profilepic varchar(100) NOT NULL,
-	password char(32) NOT NULL,
+	passwd char(32) NOT NULL,
 	about text,
 	creation_time timestamp NOT NULL,
-	status boolean NOT NULL,
+	is_active boolean NOT NULL,
 	hash_key char(32) NOT NULL,
 	primary key (user_id)
 );
 
-create table tag (
+create table tags (
 	tag_id int unsigned AUTO_INCREMENT NOT NULL,
 	name varchar(50) NOT NULL,
 	primary key (tag_id)
 );
 
-create table question (
+create table questions (
 	q_id int unsigned AUTO_INCREMENT NOT NULL,
 	title varchar(50) NOT NULL,
 	description text NOT NULL,
 	creation_time timestamp NOT NULL,
 	user_id int unsigned NOT NULL,
-	foreign key (user_id) references user(user_id) on delete cascade on update cascade,
+	foreign key (user_id) references users(user_id) on delete cascade on update cascade,
 	primary key (q_id)
 );
 
-create table answer (
+create table answers (
 	a_id int unsigned AUTO_INCREMENT NOT NULL,
 	q_id int unsigned NOT NULL,
 	user_id int unsigned NOT NULL,
 	answer_time timestamp NOT NULL,
 	answer_text  text NOT NULL,
-	foreign key (user_id) references user(user_id) on delete cascade on update cascade,
-	foreign key (q_id) references question(q_id) on delete cascade on update cascade,
+	foreign key (user_id) references users(user_id) on delete cascade on update cascade,
+	foreign key (q_id) references questions(q_id) on delete cascade on update cascade,
 	primary key (a_id)
 );
 
-create table question_tag (
+create table question_tags (
 	tag_id int unsigned NOT NULL,
 	q_id  int unsigned NOT NULL,
-	foreign key (tag_id) references tag(tag_id) on delete cascade on update cascade,
-	foreign key (q_id) references question(q_id) on delete cascade on update cascade,
+	foreign key (tag_id) references tags(tag_id) on delete cascade on update cascade,
+	foreign key (q_id) references questions(q_id) on delete cascade on update cascade,
 	primary key (tag_id,q_id)
 );
 
-create table follow(
+create table follows (
 	tag_id int unsigned NOT NULL,
 	user_id  int unsigned NOT NULL,
-	foreign key (tag_id) references tag(tag_id) on delete cascade on update cascade,
-	foreign key (user_id) references user(user_id) on delete cascade on update cascade,
+	foreign key (tag_id) references tags(tag_id) on delete cascade on update cascade,
+	foreign key (user_id) references users(user_id) on delete cascade on update cascade,
 	primary key (tag_id,user_id)
 );
