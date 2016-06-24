8b5b4f47705abcefdd1bfb1d303d7d450b08489e
Nikhil <nikhil.mittal@99acres.com>
2016-06-24 10:19:38 +0530

    Latest userid who registered...for upload

--- application/models/Users.php
+++ application/models/Users.php
@@ -39,8 +39,11 @@
 		{
 			$value = "'".$value."'";
 			// Todo: Where clause to include user_id
-			$sql = $this->conn_id->prepare("UPDATE users SET profilepic = ".$value." where user_id = 1");
-			$sql->execute();
+			$sql = $this->conn_id->query('select * from users ORDER BY user_id DESC LIMIT 1');
+			$r = $sql->fetchALL(PDO::FETCH_ASSOC);
+			// print_r($r[0]['user_id']);
+			$sql = $this->conn_id->prepare("UPDATE users SET profilepic = ".$value." where user_id = ?");
+			$sql->execute(array($r[0]['user_id']));
 			$affected_rows = $sql->rowCount();
 			echo $affected_rows;
 			return $affected_rows;
