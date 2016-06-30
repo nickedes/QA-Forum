13a6307517cd0ee69d9febb42975dd91f7d8077f
Nikhil <nikhil.mittal@99acres.com>
2016-06-30 17:52:50 +0530

    added font awesome files

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-$(function() {
+    $(function() {
+
+    highlight_errors_validate();
     // Setup form validation on the element
     $("#login_form").validate({
         // Specify the validation rules
@@ -24,6 +26,10 @@ $(function() {
                 data: $(form).serialize(),
                 dataType : 'json',
                 success: function(data) {
+                    $('#form_error').html('');
+                    $('#email_error').html('');
+                    $('#password_error').html('');
+
                     if (data.success){
                         // login successful -> redirect to home.
                         window.location.href = "home";
@@ -36,9 +42,54 @@ $(function() {
                         }
                         else
                         {
-                            $('#email_error').text(data.message);
-                            $('#password_error').text(data.message);
+                            if(typeof(data.email) != "undefined" && data.email != "" )
+                                $('#email_error').html('<br><div class="alert alert-danger">' + data.email + '</div>');
+                            if(typeof(data.password) != "undefined" && data.password != "" )
+                                $('#password_error').html('<br><div class="alert alert-danger">' + data.password + '</div>');
                         }
+                    }
+                },
+                error: function(data) {
+                    console.log(data);
+                }
+            });
+            return false;
+        },
+    });
+
+    $("#forgotpassword_form").validate({
+        // Specify the validation rules
+        rules: {
+            forgotpassword_email: "required"
+        },
+        
+        // Specify the validation error messages
+        messages: {
+            email: "Please enter a valid email address"
+        },
+        
+        submitHandler: function(form) {
+            $('#forgotpassword_error').empty();
+            $.ajax({  
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: $(form).serialize(),
+                dataType : 'json',
+                success: function(data) 
+                {
+                    if (data.success){
+                        // console.log(data.success);
+                        $('#forgotpassword_error').html('<br><div class="alert alert-success">' + data.success_message + '</div>');
+
+                        setTimeout(function(){
+                            $('#forgotpassword_error').empty();
+                            $('#forgotpasswordModal').modal('hide');
+                        }, 3000);
+                    }
+                    else
+                    {
+                        if(data.message != "")
+                            $('#forgotpassword_error').html('<br><div class="alert alert-danger">' + data.message + '</div>');
                     }
                 },
                 error: function(data) {
